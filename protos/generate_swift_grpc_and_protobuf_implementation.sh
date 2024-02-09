b313ce19d0410fd3f4a465986953621822ac5f7e
Balintus00 <traxlerbalint@gmail.com>
2024-02-09 13:04:10 +0100

    Revert "Add swift code generator script that can generate swift protobuf and grpc implementation from proto file"
    This reverts commit dfd4dd795ed0190e6fe2648c59a5d3b6910f554d.

--- protos/generate_swift_grpc_and_protobuf_implementation.sh
+++
@@ -1,6 +0,0 @@
-protoc clothing_webshop.proto \
-     --proto_path=./kotlinclothingwebshop/ \
-     --grpc-swift_opt=Client=true,Server=false,Visibility=Public \
-     --grpc-swift_out=. \
-     --swift_out=. \
-     --swift_opt=Visibility=Public
