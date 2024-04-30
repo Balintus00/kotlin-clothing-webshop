2e4944c163ab0d2094b55b237e709d8a4c6e65fe
Balintus00 <traxlerbalint@gmail.com>
2024-04-30 11:41:09 +0200

    43; Use Kermit below SLF4J instead of LOG4J2 in backend module
    - Kermit will be used on the client side too, so it might reduce a little bit the overall complexity of the project

---
+++ backend/src/main/kotlin/hu/bme/aut/ixnoyb/kotlinclothingwebshop/backend/logging/KermitServiceProvider.kt
@@ -0,0 +1,52 @@
+package hu.bme.aut.ixnoyb.kotlinclothingwebshop.backend.logging
+
+import co.touchlab.kermit.CommonWriter
+import co.touchlab.kermit.LogWriter
+import co.touchlab.kermit.Severity
+import co.touchlab.kermit.StaticConfig
+import org.slf4j.ILoggerFactory
+import org.slf4j.helpers.BasicMarkerFactory
+import org.slf4j.helpers.NOPMDCAdapter
+
+// Source: https://github.com/psh/KermitExt/blob/main/slf4j-over-kermit/src/jvmMain/kotlin/com/gatebuzz/kermit/ext/KermitServiceProvider.kt
+
+@Suppress("unused ")
+class KermitServiceProvider : org.slf4j.spi.SLF4JServiceProvider {
+    private val markerFactory = BasicMarkerFactory()
+    private val mdcAdapter = NOPMDCAdapter()
+    override fun getLoggerFactory() = ILoggerFactory {
+        Slf4jKermitLogger(it, config)
+    }
+
+    override fun getMarkerFactory() = markerFactory
+
+    override fun getMDCAdapter() = mdcAdapter
+
+    override fun getRequestedApiVersion() = "2.0.99"
+
+    override fun initialize() = Unit
+
+    companion object {
+        private val writers = mutableListOf<LogWriter>().apply {
+            add(CommonWriter())
+        }
+        var config: StaticConfig = StaticConfig(logWriterList = writers)
+
+        var minSeverity: Severity
+            get() = config.minSeverity
+            set(value) {
+                config = config.copy(minSeverity = value)
+            }
+
+        fun addWriter(writer: LogWriter) {
+            writers.add(writer)
+            config = config.copy(logWriterList = writers)
+        }
+
+        fun setWriters(vararg writer: LogWriter) {
+            writers.clear()
+            writers.addAll(writer)
+            config = config.copy(logWriterList = writers)
+        }
+    }
+}
