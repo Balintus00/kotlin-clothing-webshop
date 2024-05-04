9473251aa8f6e531c032a82be5e34eb98129346c
Balintus00 <traxlerbalint@gmail.com>
2024-05-04 15:16:51 +0200

    43; Restructure client modules
    - Moving wasmJS, JVM desktop client and Android client modules' content into the client module simplifies the structure of the project and allows better control over what should be exported to the iOS project

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -3,13 +3,14 @@ import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.application)
     alias(libs.plugins.apollo.kotlin)
     alias(libs.plugins.compose.multiplatform)
 }
 
 kotlin {
     androidTarget()
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -20,10 +21,18 @@ kotlin {
             isStatic = true
         }
     }
-    jvm()
+
+    jvm("desktop")
+
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        browser()
+        moduleName = "kotlinClothingWebshop"
+        browser {
+            commonWebpackConfig {
+                outputFileName = "kotlinClothingWebshop.js"
+            }
+        }
+        binaries.executable()
     }
 
     sourceSets {
@@ -35,9 +44,10 @@ kotlin {
                 implementation(compose.ui)
             }
         }
+
         val androidMain by getting {
             dependencies {
-                implementation(libs.androidx.core)
+                implementation(libs.androidx.activity.compose)
                 implementation(libs.compose.jetpack.preview)
             }
         }
@@ -48,22 +58,59 @@ kotlin {
 
 android {
     namespace = "hu.bme.aut.ixnoyb.kotlinclothingwebshop.client"
-
     compileSdk = libs.versions.android.sdk.compile.get().toInt()
 
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
+
     defaultConfig {
+        applicationId = "hu.bme.aut.ixnoyb.kotlinclothingwebshop.client"
         minSdk = libs.versions.android.sdk.min.get().toInt()
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.compose.jetpack.get()
+        targetSdk = libs.versions.android.sdk.compile.get().toInt()
+        versionCode = 1
+        versionName = libs.versions.kotlinClothingWebshop.get()
+
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        val javaVersionEnumName = "VERSION_${libs.versions.java.get()}"
+
+        sourceCompatibility = JavaVersion.valueOf(javaVersionEnumName)
+        targetCompatibility = JavaVersion.valueOf(javaVersionEnumName)
+    }
     dependencies {
         debugImplementation(libs.compose.jetpack.tooling)
     }
+}
+
+compose.desktop {
+    application {
+        mainClass = "hu.bme.aut.ixnoyb.kotlinclothingwebshop.client.MainKt"
+    }
+}
+
+compose.experimental {
+    web.application {}
 }
 
 apollo {
--- client/src/desktopMain/kotlin/hu/bme/aut/ixnoyb/kotlinclothingwebshop/client/main.kt
+++ client/src/desktopMain/kotlin/hu/bme/aut/ixnoyb/kotlinclothingwebshop/client/main.kt
@@ -1,4 +1,4 @@
-package hu.bme.aut.ixnoyb.kotlinclothingwebshop.desktop
+package hu.bme.aut.ixnoyb.kotlinclothingwebshop.client
 
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
